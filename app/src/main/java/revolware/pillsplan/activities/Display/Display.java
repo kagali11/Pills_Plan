bb51004efaa1fa23138312f6750714a3518fa191
Aandrej2 <and99rej@gmail.com>
2016-11-26 19:20:10 +0100

    Pridanie FE pre pridavanie alarmu/lieku.

--- app/src/main/java/revolware/pillsplan/activities/Display/Display.java
+++ app/src/main/java/revolware/pillsplan/activities/Display/Display.java
@@ -6,8 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -111,28 +109,6 @@ public class Display extends AppCompatActivity  {
             intent.putExtra("key", "true");
             startActivity(intent);
         finish();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_name) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 
